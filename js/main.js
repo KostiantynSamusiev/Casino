c7c86e996f1b89dda74a59368043028977535cfa
Kostiantyn Samusiev <djfoozyhook@gmail.com>
2023-11-27 09:08:11 +0200

    done
    done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 $(document).ready(function () {
     $("#news-slider").owlCarousel({
-        items: 2,
-        navigation: true,
         itemsDesktop: [1199, 3],
         itemsDesktopSmall: [980, 2],
         itemsMobile: [600, 1],
         navigationText: ["", ""],
+        dots: false,
+        navigation: true,
+
         pagination: true,
-        autoPlay: true
+        autoPlay: true,
+        responsive: {
+            300: {
+                items: 1,
+                nav: false
+            },
+            800: {
+                items: 2,
+                nav: false
+            },
+
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+        $(this).toggleClass('open');
+        $('.mobile-popup ').fadeToggle();
     });
 });
